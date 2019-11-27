d994bd6e5ea64d8ab60eca73f1965aab3cb30501
Nego Thales <thale.serra97@gmail.com>
2019-11-27 09:16:44 -0300

    login

--- src/main/java/br/com/casadocodigo/loja/controllers/HomeController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/HomeController.java
@@ -1,13 +1,35 @@
 package br.com.casadocodigo.loja.controllers;
 
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 
 @Controller
 public class HomeController {
 
 	@RequestMapping("/")
-	public String index() {
-		return "home";
+	public ModelAndView index() {
+		return new ModelAndView("home");
+	}
+
+	@RequestMapping(value = "validaAdmin", method = RequestMethod.POST)
+	public ModelAndView validaAdmin(HttpServletRequest req, RedirectAttributes redirectAttributes) {
+		String email = req.getParameter("email");
+		String password = req.getParameter("password");
+		System.out.println(password);
+		if(email.equals("admin@admin.com") && password.equals("010203") || email.equals("admin@admin.com.br") && password.equals("123456") ) {
+			return new ModelAndView("redirect:/produtos/");
+		}else {
+			redirectAttributes.addFlashAttribute("error", "Dados de login inválido");
+			return new ModelAndView("redirect:/");
+		}
+		
 	}
 }
